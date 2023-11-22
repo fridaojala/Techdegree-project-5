82982ee63611534da30cca2ae470bf46cc33e9c4
fridaojala <146091342+fridaojala@users.noreply.github.com>
2023-11-22 14:17:41 +0100

    formation commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 
 baguetteBox.run('.gallery');
 
-let imgSearch = document.getElementById('searchfield');
+const imgSearch = document.getElementById('searchfield');
 imgSearch.addEventListener('keyup', e => {
     let currentValue = e.target.value.toLowerCase();
-    let captions = document.querySelectorAll( '[data-caption]' );
+    const captions = document.querySelectorAll( '[data-caption]' );
     captions.forEach( caption => {
         if (caption.getAttribute('data-caption').toLowerCase().includes( currentValue )) {
             caption.style.display = 'block';
